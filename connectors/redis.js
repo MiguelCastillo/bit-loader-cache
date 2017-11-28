d9170d4ac5634e593b17c88f4c38a2aa5fe18bef
Miguel Castillo <manchagnu@gmail.com>
2017-11-28 12:50:06 -0500

    Adding a redis connector to allow in memory storage of the cached data

---
+++ connectors/redis.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const IConnector = require("./iconnector");
+const redis = require("redis");
+const utils = require("belty")
+
+class Redis extends IConnector {
+  constructor(options) {
+    super();
+    this.options = Object.assign({}, options);
+    this.client = redis.createClient(utils.omit(options, ["watch"]));
+  }
+
+  flush() {
+    if (!this.options.watch) {
+      this.client.quit();
+    }
+  }
+
+  get(id) {
+    return toPromise(this.client.get.bind(this.client, id));
+  }
+
+  set(id, data) {
+    return toPromise(this.client.set.bind(this.client, id, JSON.stringify(data))).then(() => null);
+  }
+}
+
+function toPromise(method) {
+  return new Promise((resolve, reject) => method((err, data) => err ? reject(err) : resolve(data)));
+}
+
+module.exports = function(options) {
+  return new Redis(options);
+};
